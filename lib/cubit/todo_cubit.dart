a194e2241eaa98f2167b5cfa1230a922836bc53b
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-05-21 20:22:33 +0800

    [2] Add function AddTodo
    AddTodo function to add todo list to main screen and the function in button add

--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -3,4 +3,19 @@ import 'package:todo_list/todo_model.dart';
 
 class TodoCubit extends Cubit<List<Todo>> {
   TodoCubit() : super([]);
+
+  void addTodo(String title) {
+    final todo = Todo(
+      name: title,
+      createdAt: DateTime.now(),
+    );
+
+    emit([...state, todo]);
+  }
+
+  @override
+  void onChange(Change<List<Todo>> change) {
+    super.onChange(change);
+    print('TodoCubit - $change');
+  }
 }
